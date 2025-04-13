f0a86335ff594ad8510030384b73a889a8147bfc
Kshitij <kshitij.tanwar2002@gmail.com>
2025-04-13 22:23:59 +0530

    feat: add Work Sans font files and integrate local font loading in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,56 @@
 import type { Metadata } from "next";
-import { Work_Sans } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const workSans = Work_Sans({
+const workSans = localFont({
+    src: [
+        {
+            path: "./fonts/WorkSans-Black.ttf",
+            weight: "900",
+            style: "normal",
+        },
+        {
+            path: "./fonts/WorkSans-ExtraBold.ttf",
+            weight: "800",
+            style: "normal",
+        },
+        {
+            path: "./fonts/WorkSans-Bold.ttf",
+            weight: "700",
+            style: "normal",
+        },
+        {
+            path: "./fonts/WorkSans-SemiBold.ttf",
+            weight: "600",
+            style: "normal",
+        },
+        {
+            path: "./fonts/WorkSans-Medium.ttf",
+            weight: "500",
+            style: "normal",
+        },
+        {
+            path: "./fonts/WorkSans-Regular.ttf",
+            weight: "400",
+            style: "normal",
+        },
+        {
+            path: "./fonts/WorkSans-Thin.ttf",
+            weight: "300",
+            style: "normal",
+        },
+        {
+            path: "./fonts/WorkSans-Light.ttf",
+            weight: "200",
+            style: "normal",
+        },
+        {
+            path: "./fonts/WorkSans-ExtraLight.ttf",
+            weight: "100",
+            style: "normal",
+        },
+    ],
     variable: "--font-work-sans",
-    subsets: ["latin"],
-    weight: ["400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
