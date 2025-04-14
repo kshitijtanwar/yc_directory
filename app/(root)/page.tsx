1eeb43433beccbaa8f11592b1d3b93fb299e67b6
Kshitij <kshitij.tanwar2002@gmail.com>
2025-04-14 15:37:55 +0530

    feat: update Home page layout and styles, and modify metadata for YC Directory

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
-        <>
-            <h1 className="text-2xl">Home</h1>
-        </>
+        <div className="pink-container h-screen">
+            <h1 className="heading">Home</h1>
+        </div>
     );
 }
