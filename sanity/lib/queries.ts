08d849863bc66ba26a4a049b430bdf1b9723b71f
Kshitij <kshitij.tanwar2002@gmail.com>
2025-04-16 02:39:52 +0530

    chore: added search

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,18 +1,16 @@
 import { defineQuery } from "next-sanity";
 
-export const STARTUP_QUERY = defineQuery(`*[_type == "startup"]{
-    _id,
-    title,
+export const STARTUP_QUERY =
+    defineQuery(`*[_type == "startup" && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc) {
+    _id, 
+    title, 
     slug,
-    author->{
-        _id,
-        name,
-        image,
-        bio
-    },
+    _createdAt,
+    author -> {
+      _id, name, image, bio
+    }, 
     views,
     description,
     category,
     image,
-    _createdAt,
-}`);
+  }`);
