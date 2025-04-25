ff51c2aebb34bb690c80386bf5f89a44a96ac4ff
Kshitij <kshitij.tanwar2002@gmail.com>
2025-04-26 04:06:30 +0530

    feat: add View and Ping components, implement skeleton loading, and enhance startup page with view tracking

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -5,6 +5,9 @@ import { StartupType } from "@/types/definitions";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import markdownit from "markdown-it";
+import { Suspense } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
+import View from "@/components/View";
 
 const md = markdownit();
 
@@ -71,6 +74,10 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
                         <p className="no-result">No details provided.</p>
                     )}
                 </div>
+                <hr className="divider" />
+                <Suspense fallback={<Skeleton className="view_skeleton" />}>
+                    <View id={id} />
+                </Suspense>
             </section>
         </>
     );
